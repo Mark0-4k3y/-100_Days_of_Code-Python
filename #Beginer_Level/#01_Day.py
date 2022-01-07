4401f924c6abb90df88f8b67240d6ea9f35cef12
Keshav Vashishta_4k3y <keshavvashisth123@gmail.com>
2022-01-07 22:01:20 +0530

    initial commit

---
+++ #Beginer_Level/#01_Day.py
@@ -0,0 +1,15 @@
+# This is the first project or task in 100 days of code in python
+# In this I will make a Band Generator app or code:
+
+# Welcome the user to the application
+print("Hey, Welcome to the Band Name Generator app.")
+
+# Ask user to for input about there city name:
+cityName = input("Please, Enter your city name in which your grew up :)\n")
+
+# Ask user to input there Pet name
+petName = input("Please, enter your pet name, in case you don't have just assume ;)\n")
+
+# giving user the Band name based on their inputs
+print("Your band name should be: " + cityName + " " + petName + "~" + str(len(cityName + petName) - 1))
+
