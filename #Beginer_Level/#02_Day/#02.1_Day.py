bfe1d01261d22f11cb8a08350ca15aaf28d32305
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-01-08 10:46:08 +0530

    Commiting again

---
+++ #Beginer_Level/#02_Day/#02.1_Day.py
@@ -0,0 +1,9 @@
+# In this I will make an BMI(Body mass Index calculator)
+# It is given as: body mass/height*height
+
+# Taking input of body weight and height
+weight = int(input("Enter you body weight Sir/Ma'am in kilogram(kg): "))
+height = float(input("Enter you height Sir/Ma'am in meter(m): "))
+
+# Giving output as BMI
+print("Your BMI is: ", str(int(weight/height ** 2))+" kg/m^2")
