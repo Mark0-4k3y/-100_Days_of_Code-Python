a8de7b334417846af276f4086ce871d58723ff22
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-02-11 23:06:42 +0530

    Day 4

---
+++ Beginner_Level/#05_Day/#05.1_Day.py
@@ -0,0 +1,16 @@
+#This is the fifth day of coding, in which I have to make an average height predictor application.
+
+#First I have to take the list of heights of students.
+heightList = input("Enter the height of the students in 'cm' separated by space:\n").split(' ')
+
+#Now for finding the average of students height we have to do some calculations.
+num = 0
+for height in heightList:
+    num = num + int(height)
+
+print(round(num/len(heightList)))
+
+#There is also another way to find out the sum of the list
+#Python has inbuilt sum function which will return the sum of the list
+numList = [150, 450, 850, 260, 2001]
+print(sum(numList)/len(numList))
---
+++ Beginner_Level/#05_Day/#05.2_Day.py
@@ -0,0 +1,36 @@
+#This is the second project of day 5, in which I have to find and return the maximum score from the student subjects.
+
+#First I will have to take the subject name and the marks of respective subject as input
+subList = input('Enter the name of subjects separated by space: \n').split(' ')
+marksList = []
+for subject in subList:
+    marksList.append(int(input(f"Enter the marks of the {subject}: ")))
+
+maxMarks = marksList[0]
+index = 0
+sub = 0
+for marks in marksList:
+    if marks > maxMarks:
+        maxMarks = marks
+        sub = index
+    index += 1
+
+print(f'The student have maximum marks in {subList[sub]}, which are: ', maxMarks, '\n')
+
+
+#What if the student have same maximum marks in more than one subject
+indexList = []
+index = 0
+for mark in marksList:
+    if maxMarks == mark:
+        indexList.append(index)
+    index += 1
+
+print("The students have maximum marks in more than one subject which are: ")
+for i in indexList:
+    print(subList[i], end=', ')
+print(f"and the maximum marks is: {maxMarks}")
+
+
+#There are also inbuilt function in python which will return maximum and minimum value
+print(max(marksList), " and", min(marksList))
